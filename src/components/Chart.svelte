6bf0d73658c7384dfa8c30b6e4db9ec0ea3e9af6
Ioannis Nezis <ioannis@nezis.de>
2024-05-24 22:10:56 +0200

    wip: starting chart ui

--- src/components/Chart.svelte
+++ src/components/Chart.svelte
@@ -13,6 +13,10 @@
     } from '$lib/types';
     import LineChart from './charts/LineChart.svelte';
     import LoadingCard from './loadingCard.svelte';
+    import Maximize from '$lib/svg/maximize.svelte';
+    import Reload from '$lib/svg/reload.svelte';
+    import WrenchScrewdriver from '$lib/svg/wrench-screwdriver.svelte';
+    import CodeBracket from '$lib/svg/code-bracket.svelte';
 
     export let title: string;
     export let dataUrl: string;
@@ -48,10 +52,28 @@
     class="static bg-zinc-300 dark:bg-zinc-700 rounded-lg shadow-lg hover:shadow-xl p-3 flex flex-col w-full"
 >
     <div class="flex justify-between items-center">
-        <div></div>
-        <h1 class="underline text-black dark:text-white">{title}</h1>
-        <div>
-            <ChartSettings />
+        <h1 class="underline text-black dark:text-white ms-5">{title}</h1>
+        <div class="flex flex-row space-x-1">
+            <button
+                class="rounded-md p-1 bg-blue-300 hover:bg-blue-500 dark:bg-purple-950 dark:hover:bg-purple-800 text-black dark:text-white hover:cursor-pointer"
+            >
+                <WrenchScrewdriver></WrenchScrewdriver>
+            </button>
+            <button
+                class="rounded-md p-1 bg-blue-300 hover:bg-blue-500 dark:bg-purple-950 dark:hover:bg-purple-800 text-black dark:text-white hover:cursor-pointer"
+            >
+                <CodeBracket></CodeBracket>
+            </button>
+            <button
+                class="rounded-md p-1 bg-blue-300 hover:bg-blue-500 dark:bg-purple-950 dark:hover:bg-purple-800 text-black dark:text-white hover:cursor-pointer"
+            >
+                <Reload></Reload>
+            </button>
+            <button
+                class="rounded-md p-1 bg-blue-300 hover:bg-blue-500 dark:bg-purple-950 dark:hover:bg-purple-800 text-black dark:text-white hover:cursor-pointer"
+            >
+                <Maximize></Maximize>
+            </button>
         </div>
     </div>
     <LoadingCard {loadingState}></LoadingCard>
