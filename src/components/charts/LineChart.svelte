30903ddb8f18cc3f52a152bab3517b62848e88ba
Ioannis Nezis <ioannis@nezis.de>
2024-05-24 21:25:21 +0200

    adding legend to line plot

--- src/components/charts/LineChart.svelte
+++ src/components/charts/LineChart.svelte
@@ -16,7 +16,6 @@
             Object.fromEntries(structure.columns.map((col) => [col.name, data[col.name][index]]))
         );
     }
-    console.log(lineData);
 
     onMount(() => {
         width = node.clientWidth;
@@ -31,29 +30,37 @@
     let marginLeft = 40;
     let marginRight = 40;
 
+    const color = d3.scaleOrdinal(d3.schemeSet1);
+    let visible = config.yAxisColumns.map(() => true);
+
     let gx: SVGGElement;
     let gy: SVGGElement;
 
     // NOTE: yValues can only be number[], i just dont know how to convince ts jet
-    const yValues: number[] = config.yAxisColumns.map((col) => data[col]).flat();
-    let [minValue, maxValue] = d3.extent(yValues);
+    $: yValues = config.yAxisColumns
+        .filter((_col, i) => visible[i])
+        .map((col) => data[col])
+        .flat();
+    $: [minValue, maxValue] = d3.extent(yValues);
 
     // NOTE: xValues is a bit more tricky. But its the same issue as with yValues.
     const xValues: Date = data[config.xAxisColumn];
     $: x = d3.scaleTime(d3.extent(xValues), [marginLeft, width - marginRight]);
     $: d3.select(gx).call(d3.axisBottom(x));
     $: y = d3.scaleLinear([minValue ?? 0, maxValue ?? 1], [height - marginBottom, marginTop]);
-    $: d3.select(gy).call(d3.axisLeft(y));
+    $: d3.select(gy).transition().call(d3.axisLeft(y));
 
     let circle: string | null = d3.symbol(d3.symbolCircle, config.nodeRadius)();
 
     let lines: [string, [number, number][]][] = [];
 
     function generateLine(column: string): string {
-        return d3.line(
-            (d) => x(d[config.xAxisColumn]),
-            (d) => y(d[column])
-        )(lineData);
+        return d3
+            .line(
+                (d) => x(d[config.xAxisColumn]),
+                (d) => y(d[column])
+            )
+            .curve(d3.curveCatmullRom.alpha(config.tention))(lineData);
     }
 
     function generateNodes(column: string): [number, number][] {
@@ -85,24 +92,36 @@
             stroke-width="1"
             transform="translate({marginLeft},0)"
         />
-        {#each lines as [line, nodes]}
-            <path
-                fill="none"
-                class="text-red-600 dark:text-red-800"
-                stroke="currentcolor"
-                stroke-width={config.strokeWeight}
-                d={line}
-            ></path>
-            {#each nodes as [xPos, yPos]}
+        {#each lines as [line, nodes], index}
+            {#if visible[index]}
                 <path
-                    class="text-black dark:text-white"
-                    stroke="currentcolor"
-                    fill="currentcolor"
+                    fill="none"
+                    stroke={color(config.yAxisColumns[index])}
                     stroke-width={config.strokeWeight}
-                    transform="translate({xPos}, {yPos})"
-                    d={circle}
+                    d={line}
                 ></path>
-            {/each}
+                {#each nodes as [xPos, yPos]}
+                    <path
+                        class="text-black dark:text-white"
+                        stroke="currentcolor"
+                        fill="currentcolor"
+                        stroke-width={config.strokeWeight}
+                        transform="translate({xPos}, {yPos})"
+                        d={circle}
+                    ></path>
+                {/each}
+            {/if}
         {/each}
     </svg>
+</div>
+<div class="flex justify-center dark:text-white">
+    {#each config.yAxisColumns as col, index}
+        <button
+            class="border-4 rounded-lg px-2 mx-2"
+            style="border-color: {color(col)};"
+            on:click={() => (visible[index] = !visible[index])}
+        >
+            {col}
+        </button>
+    {/each}
 </div>
